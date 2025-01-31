850635f721c6c43db9f53110713084a24e3377e6
manojkumar211 <mk.chinthakindi@gmail.com>
2025-01-31 09:54:58 +0530

    commit

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ st.header("My New Generative AI project")
 st.logo('C:/Users/Archana Siripuram/Desktop/ge2.png',size="large")
 st.image('C:/Users/Archana Siripuram/Desktop/ge1.jpg')
 st.chat_input("Enter your Query Here :")
-st.button("Submit :")
 
 class pdf_loader:
     pdf_doc=pdf_load.load()
