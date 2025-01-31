ea7d5267eca0034538bb2d5235bbda024a89cdfb
manojkumar211 <mk.chinthakindi@gmail.com>
2025-01-31 09:41:51 +0530

    commit

--- app.py
+++ app.py
@@ -7,30 +7,30 @@ import streamlit as st
 
 st_file=st.file_uploader("Upload a PDF file :",type="pdf")
 
-if st_file is not None:
-    pdf_load=PyPDFLoader(st_file)
-    pdf_text=""
-
-    for page in pdf_load.load():
-        pdf_text+=page.page_content
-    st.write(pdf_text)
-
 
 st.header("My New Generative AI project")
 st.logo('C:/Users/Archana Siripuram/Desktop/ge2.png',size="large")
 st.image('C:/Users/Archana Siripuram/Desktop/ge1.jpg')
 
-"""class pdf_loader:
-    pdf_doc=pdf_load.load()
+class pdf_loader:
+    
+    if st_file is not None:
+        pdf_load=PyPDFLoader(st_file)
+        pdf_doc=pdf_load.load()
+        pdf_text=""
+
+    for page in pdf_load.load():
+        pdf_text+=page.page_content
+    st.write(pdf_text)
 
     def __init__(self, pdf_doc):
         self.pdf_doc=pdf_doc
 
     def pdf_load_doc(self):
-        return self.pdf_doc"""
+        return self.pdf_doc
         
 
-"""print(pdf_loader.pdf_doc[0].page_content)"""
+print(pdf_loader.pdf_doc[0].page_content)
 
 # Text splitter
 
@@ -40,7 +40,7 @@ class text_splitter:
         chunk_size=2000,
         chunk_overlap=500
 )
-    cts_doc=cts_gen.split_documents(pdf_text)
+    cts_doc=cts_gen.split_documents(pdf_loader.pdf_doc)
     def __init__(self,cts_gen,cts_doc):
         self.cts_gen=cts_gen
         self.cts_doc=cts_doc
