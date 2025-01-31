6b80c86107407890fe3688cfc2814fc8d45de5d3
manojkumar211 <mk.chinthakindi@gmail.com>
2025-01-31 09:49:07 +0530

    commit

--- app.py
+++ app.py
@@ -5,31 +5,33 @@ import streamlit as st
 
 # Load the PDF file
 
-st_file=st.file_uploader("Upload a PDF file :",type="pdf")
+""""st_file=st.file_uploader("Upload a PDF file :",type="pdf")
 
+if st_file is not None:
+    pdf_load=PyPDFLoader(st_file)
+    pdf_text=""
+
+    for page in pdf_load.load():
+        pdf_text+=page.page_content
+    st.write(pdf_text)""""
+
+pdf_load=PyPDFLoader("C:/Users/Archana Siripuram/Desktop/attention-is-all-you-need-Paper.pdf")
 
 st.header("My New Generative AI project")
 st.logo('C:/Users/Archana Siripuram/Desktop/ge2.png',size="large")
 st.image('C:/Users/Archana Siripuram/Desktop/ge1.jpg')
 
 class pdf_loader:
-    
-    if st_file is not None:
-        pdf_load=PyPDFLoader(st_file)
-        pdf_doc=pdf_load.load()
-        pdf_text=""
+    pdf_doc=pdf_load.load()
 
-    for page in pdf_load.load():
-        pdf_text+=page
-
-    def __init__(self, pdf_text):
-        self.pdf_text=pdf_text
+    def __init__(self, pdf_doc):
+        self.pdf_doc=pdf_doc
 
     def pdf_load_doc(self):
-        return self.pdf_text
+        return self.pdf_doc
         
 
-print(pdf_loader.pdf_text[0].page_content)
+print(pdf_loader.pdf_doc[0].page_content)
 
 # Text splitter
 
@@ -39,7 +41,7 @@ class text_splitter:
         chunk_size=2000,
         chunk_overlap=500
 )
-    cts_doc=cts_gen.split_documents(pdf_loader.pdf_text)
+    cts_doc=cts_gen.split_documents(pdf_loader.pdf_doc)
     def __init__(self,cts_gen,cts_doc):
         self.cts_gen=cts_gen
         self.cts_doc=cts_doc
