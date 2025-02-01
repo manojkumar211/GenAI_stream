57b7e73a34ac18c028b5d848b4d503536ca3a363
manojkumar211 <mk.chinthakindi@gmail.com>
2025-02-01 12:36:02 +0530

    commit

--- main.py
+++ main.py
@@ -54,15 +54,15 @@ if st.button("Document Embedding"):
 
 import time
 
-prompt1=st.text_input("Enter your Question regards Attention mechanism :")
+query=st.text_input("Enter your Question regards Attention mechanism :")
 
 
-if prompt1:
+if query:
     document_chain=create_stuff_documents_chain(llm,prompt)
     retriever=st.session_state.vector_store.as_retriever()
     retriever_chain=create_retrieval_chain(retriever,document_chain)
     start=time.process_time()
-    response=retriever_chain.invoke({'input':prompt1})
+    response=retriever_chain.invoke({'input':query})
     print("response time :",time.process_time()-start)
     st.write(response['answer'])
 
